dab059012380e3246ca9b52169ca1764b5dc4e2c
Ning Yu <ningyu@vulcan07.umiacs.umd.edu>
2019-10-13 22:28:28 -0400

    update

---
+++ SNGAN/datasets/celeba.py
@@ -0,0 +1,46 @@
+import numpy as np
+from PIL import Image
+import chainer
+import random
+import scipy.misc
+
+
+class CelebADataset(chainer.dataset.DatasetMixin):
+    def __init__(self, path, root, size=128, resize_method='bilinear', augmentation=False, crop_ratio=1.0):
+        self.base = chainer.datasets.LabeledImageDataset(path, root)
+        self.size = size
+        self.resize_method = resize_method
+        self.augmentation = augmentation
+        self.crop_ratio = crop_ratio
+
+    def __len__(self):
+        return len(self.base)
+
+    def transform(self, image):
+        image = image / 128. - 1.
+        image += np.random.uniform(size=image.shape, low=0., high=1. / 128)
+        return image
+
+    def get_example(self, i):
+        image, label = self.base[i]
+        image = self.transform(image)
+        return image, label
+
+
+if __name__ == "__main__":
+    import glob, os, sys
+
+    root_path = sys.argv[1]
+
+    count = 0
+    n_image_list = []
+    filenames = glob.glob(root_path + '/*.png')
+    for filename in filenames:
+        filename = filename.split('/')
+        n_image_list.append([filename[-1], 0])
+        count += 1
+        if count % 10000 == 0:
+            print(count)
+    print("Num of examples:{}".format(count))
+    n_image_list = np.array(n_image_list, np.str)
+    np.savetxt('celeba_align_png_cropped_image_list.txt', n_image_list, fmt="%s")
---
+++ SNGAN/evaluations/gen_images.py
@@ -0,0 +1,59 @@
+import os, sys, time
+import shutil
+import numpy as np
+import argparse
+import chainer
+from PIL import Image
+
+base = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(base, '../'))
+from evaluation import gen_images_with_condition
+import yaml
+import source.yaml_utils as yaml_utils
+
+
+def load_models(config):
+    gen_conf = config.models['generator']
+    gen = yaml_utils.load_model(gen_conf['fn'], gen_conf['name'], gen_conf['args'])
+    return gen
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_path', type=str, default='configs/base.yml')
+    parser.add_argument('--gpu', '-g', type=int, default=0)
+    parser.add_argument('--results_dir', type=str, default='./results/gans')
+    parser.add_argument('--snapshot', type=str, default='')
+    parser.add_argument('--rows', type=int, default=1)
+    parser.add_argument('--columns', type=int, default=1)
+    parser.add_argument('--classes', type=int, nargs="*", default=None)
+    parser.add_argument('--seed', type=int, default=1234)
+    parser.add_argument('--num_pngs', type=int, default=100)
+    args = parser.parse_args()
+    chainer.cuda.get_device_from_id(args.gpu).use()
+    config = yaml_utils.Config(yaml.load(open(args.config_path)))
+    # Model
+    gen = load_models(config)
+    gen.to_gpu(args.gpu)
+    out = args.results_dir
+    chainer.serializers.load_npz(args.snapshot, gen)
+    np.random.seed(args.seed)
+    classes = tuple(args.classes) if args.classes is not None else np.arange(0, gen.n_classes, dtype=np.int32)
+    for c in classes:
+        for png_idx in range(args.num_pngs):
+            print('Generating png %d / %d...' % (png_idx, args.num_pngs))
+            with chainer.using_config('train', False), chainer.using_config('enable_backprop', False):
+                x = gen_images_with_condition(gen, c=c, n=args.rows * args.columns, batchsize=args.rows * args.columns)
+            _, _, h, w = x.shape
+            x = x.reshape((args.rows, args.columns, 3, h, w))
+            x = x.transpose(0, 3, 1, 4, 2)
+            x = x.reshape((args.rows * h, args.columns * w, 3))
+
+            save_path = os.path.join(out, 'SNGAN_%08d.png' % png_idx)
+            if not os.path.exists(out):
+                os.makedirs(out)
+            Image.fromarray(x).save(save_path)
+
+
+if __name__ == '__main__':
+    main()
